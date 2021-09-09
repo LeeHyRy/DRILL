4c11221422ce66ef8d03e4e7d1c454888254243a
LeeHyRy <ukrk774@kpu.ac.kr>
2021-09-09 19:54:49 +0900

    Drill 03 완성
    2020180032 이형룡

---
+++ 03/name.py
@@ -0,0 +1,89 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-300, 0)
+turtle.pendown()
+turtle.circle(40)
+
+turtle.penup()
+turtle.goto(-240, 100)
+turtle.pendown()
+turtle.goto(-240, -20)
+
+turtle.penup()
+turtle.goto(-180, 140)
+turtle.pendown()
+turtle.forward(50)
+
+turtle.penup()
+turtle.goto(-200, 120)
+turtle.pendown()
+turtle.forward(90)
+
+turtle.penup()
+turtle.goto(-155, 50)
+turtle.pendown()
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(-110, 70)
+turtle.pendown()
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(-110, 90)
+turtle.pendown()
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(-90, 140)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(100)
+
+turtle.penup()
+turtle.goto(-140, 0)
+turtle.pendown()
+turtle.circle(30)
+
+turtle.penup()
+turtle.goto(-50, 140)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(70)
+
+turtle.right(90)
+turtle.forward(30)
+
+turtle.right(90)
+turtle.forward(70)
+
+turtle.left(90)
+turtle.forward(30)
+
+turtle.left(90)
+turtle.forward(70)
+
+turtle.penup()
+turtle.goto(-30, 65)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(0, 65)
+turtle.pendown()
+turtle.forward(20)
+
+turtle.penup()
+turtle.goto(-55, 45)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(80)
+
+turtle.penup()
+turtle.goto(-15, -30)
+turtle.pendown()
+turtle.circle(30)
+
+turtle.exitonclick()
