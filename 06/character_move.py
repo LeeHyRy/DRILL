8da40fb317ceb4f03ae227690a6e00f50837383b
LeeHyRy <ukrk774@kpu.ac.kr>
2021-09-16 22:56:53 +0900

    Drill 06 완성
    2020180032 이형룡
    https://www.youtube.com/watch?v=HbETs5jcyog

---
+++ 06/character_move.py
@@ -0,0 +1,51 @@
+from pico2d import *
+import math
+
+def move_rect():
+    x = 0
+    y = 30
+    while x < 790:
+        clear_canvas()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x += 15
+        delay(0.01)
+    while y < 590:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw_now(x, y)
+        y += 15
+        delay(0.01)
+    while x > 10:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw_now(x, y)
+        x -= 15
+        delay(0.01)
+    while y > 30:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.draw_now(x, y)
+        y -= 15
+        delay(0.01)
+
+def move_circle():
+    r = 0
+    t = 0
+    while t < 2*math.pi:
+        clear_canvas()
+        grass.draw_now(400, 30)
+        character.draw_now(math.sin(t)*200+400, -math.cos(t)*200+300)
+        t += 0.1
+        delay(0.01)
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+move_rect()
+move_circle()
+
+close_canvas()
+
